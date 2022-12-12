4eeb85a909cf331bb0c72d08d1b294b4b7b6ff13
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2022-12-12 18:38:24 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,48 +1,40 @@
 import Notiflix from 'notiflix';
 
-const refForm = document.querySelector("form");
-const refInputs = document.querySelectorAll("input");
-const delayRef = refInputs[0];
-const stepRef =  refInputs[1];
-const amountRef = refInputs[2];
-const btnSubmit= document.querySelector("button");
 
 
- function createPromise(position, delay) {
-    let promise =  new Promise((resolve, reject) => {
-      const shouldResolve = Math.random() > 0.3;
 
-      setTimeout(() => {
-      if (shouldResolve) {   
-       resolve({ position, delay});
-      } 
-       reject({ position, delay});
-     }, delay);
-      });
-      return promise;
-    };
+const form = document.querySelector(".form");
 
-    
-refForm.addEventListener("input", () => {
-  let delay = parseInt(delayRef.value);
-  let step = parseInt(stepRef.value);
-  let amount = parseInt(amountRef.value);
+form.addEventListener('submit', handleSubmit);
 
- refForm.addEventListener("submit", (event) =>{
-   event.preventDefault();
-   console.log(delay, step, amount)
-  for (let i = 1; i <= amount; i+=1) {
-   createPromise(i, delay)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+function createPromise(position, delay) {
+
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve("Success! Value passed to resolve function");
+        Notiflix.Notify.success(`Fulfilled promise ${position+1} in ${delay}ms`);
+      } else {
+        reject("Error! Error passed to reject function");
+        Notiflix.Notify.failure(`Rejected promise  ${position+1} in ${delay}ms`);
+      }
+    }, delay);
   })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }); 
-  delay += step
-    };
- })
-}); 
+  };
 
+function handleSubmit(event) {
 
+  event.preventDefault();
+  let delayFirst = Number(form.delay.value);
+  const delayStep = Number(form.step.value);
+  const promisesAmount = Number(form.amount.value);
 
+  for (let position = 0; position <= promisesAmount - 1; position += 1) {
+    const delay = delayFirst + (position * delayStep);
+    console.log(delay);
+    console.log(position);
+
+    createPromise(position, delay);
+  }
+}
